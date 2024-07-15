657c4597d8d22899bf68191e5e4f89811fb110bd
EdgarDaza <edgar.daza@utp.ac.pa>
2024-07-15 17:21:37 -0500

    change access denied

--- src/app/access-denied/access-denied.page.ts
+++ src/app/access-denied/access-denied.page.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+  IonGrid,
+  IonRow,
+  NavController,
+  IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-access-denied',
   templateUrl: './access-denied.page.html',
   styleUrls: ['./access-denied.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    IonGrid,
+  IonRow,
+  IonButton]
 })
 export class AccessDeniedPage implements OnInit {
 
